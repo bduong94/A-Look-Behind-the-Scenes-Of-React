494f8d22fbb58dd95a23c8a4c4a7381b22d76041
Brian Duong <brianminhduong@gmail.com>
2022-07-04 21:16:54 -0400

    Add state to hide or show paragraph element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
     </div>
   );
 }
